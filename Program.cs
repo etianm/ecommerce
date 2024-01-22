0dd85146ee28f8f8cd6e18edb3efdb0f4973d035
etianm <155981372+etianm@users.noreply.github.com>
2024-01-22 09:20:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Order;
+{
+    public static void Main()
+    {
+        string Customer;
+        DateTime Date;
+        decimal Total;
+    }
+}
+
+public class PaymentSystem;
+{
+    public static void Main()
+    {
+        int Suma;
+        Console.WriteLine("Write the Amount of Money here: \n\n");
+        Suma = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Choose payment method:\n");
+        Console.WriteLine("- Card -\n");
+        Console.WriteLine("- PayPal -\n");
+        Console.WriteLine("- Wallet Crypto -\n\n");
+        Customer = Console.ReadLine();
+    }
+}
+
+public class OrderProcessor;
+{
+    public static class FulfillOrder()
+    {
+        Console.WriteLine("")
+    }
+}
+
